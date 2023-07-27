b952b48d10a0cf96332bc9c0f2f9bd04b2218c34
Piotr Jaworski <piotr.jaworski@rtbhouse.com>
2023-07-27 12:42:19 +0200

    Server-side form validation I

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -41,6 +41,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 
 	// in order to enable images loading we have to run a file server
 	fileServer := http.FileServer(http.Dir("./static/"))
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/piotrj-rtbh/bookings/internal/config"
+	"github.com/piotrj-rtbh/bookings/internal/forms"
 	"github.com/piotrj-rtbh/bookings/internal/models"
 	"github.com/piotrj-rtbh/bookings/internal/render"
 )
@@ -60,7 +61,15 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		// for GET we make the Form object be empty
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 // Generals renders the room page
