bb09bcf094c82a742c2d93c1b7ace8453623c6c2
Piotr Jaworski <piotr.jaworski@rtbhouse.com>
2023-08-17 13:15:48 +0200

    Inserting Room Restrictions

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -90,11 +90,13 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	endDate, err := time.Parse(layout, ed)
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 
 	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 
 	reservation := models.Reservation{
@@ -133,12 +135,26 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// write the information to the DB
-	err = m.DB.InsertReservation(reservation)
+	newReservationID, err := m.DB.InsertReservation(reservation)
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 
 	// we have to put room restrictions as well!
+	restriction := models.RoomRestriction{
+		StartDate:     startDate,
+		EndDate:       endDate,
+		RoomID:        roomID,
+		ReservationID: newReservationID,
+		RestrictionID: 1,
+	}
+
+	err = m.DB.InsertRoomRestriction(restriction)
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
 
 	// store the reservation object in the session
 	m.App.Session.Put(r.Context(), "reservation", reservation)
--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -14,11 +14,13 @@ func (m *postgresDBRepo) AllUsers() bool {
 }
 
 // InsertReservation inserts a reservation into the database
-func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
+func (m *postgresDBRepo) InsertReservation(res models.Reservation) (int, error) {
 	// in case the user loses his connection while a transaction is being executed in the backend
 	// then make this connection timeout after 3 seconds
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
+	var newID int
 
 	stmt := `insert into reservations (first_name, last_name, email, phone, start_date, 
 					 end_date, room_id, created_at, updated_at)
@@ -27,7 +29,7 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 
 	// there is m.DB.Exec(stmt, params...) but with context version we assure we timeout
 	// the connection when something wrong happens
-	_, err := m.DB.ExecContext(ctx, stmt,
+	err := m.DB.QueryRowContext(ctx, stmt,
 		res.FirstName,
 		res.LastName,
 		res.Email,
@@ -37,10 +39,37 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 		res.RoomID,
 		time.Now(),
 		time.Now(),
+	).Scan(&newID)
+
+	if err != nil {
+		return 0, err
+	}
+	return newID, nil
+}
+
+// InsertRoomRestriction inserts a room restriction into the database
+func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `insert into room_restrictions (start_date, end_date, room_id, reservation_id,
+						created_at, updated_at, restriction_id)
+						values
+						($1, $2, $3, $4, $5, $6, $7)`
+
+	_, err := m.DB.ExecContext(ctx, stmt,
+		r.StartDate,
+		r.EndDate,
+		r.RoomID,
+		r.ReservationID,
+		time.Now(),
+		time.Now(),
+		r.RestrictionID,
 	)
 
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
