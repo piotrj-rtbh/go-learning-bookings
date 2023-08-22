1a0723ebd3467361570e81cd8fb5edc8c029f3e6
Piotr Jaworski <piotr.jaworski@rtbhouse.com>
2023-08-22 12:58:34 +0200

    Database functions for editing a reservation

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -136,3 +136,15 @@ func (m *testDBRepo) GetReservationByID(id int) (models.Reservation, error) {
 
 	return res, nil
 }
+
+func (m *testDBRepo) UpdateReservation(u models.Reservation) error {
+	return nil
+}
+
+func (m *testDBRepo) DeleteReservation(id int) error {
+	return nil
+}
+
+func (m *testDBRepo) UpdateProcessedForReservation(id, processed int) error {
+	return nil
+}
