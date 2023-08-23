3434e1ea5e8e7d6045f40f33753d12170d678a0a
Piotr Jaworski <piotr.jaworski@rtbhouse.com>
2023-08-23 12:58:53 +0200

    Reservation Calendar II

--- internal/render/render.go
+++ internal/render/render.go
@@ -15,11 +15,29 @@ import (
 )
 
 var functions = template.FuncMap{
-	"humanDate": HumanDate, // in template, using {{humanDate .Prop}} will format the date by this function: HumanDate
+	"humanDate":  HumanDate,  // in template, using {{humanDate .Prop}} will format the date by this function: HumanDate
+	"formatDate": FormatDate, // {{formatDate .Prop}} will format a date as month name string
+	"iterate":    Iterate,
+	"add":        Add,
 }
 
 var app *config.AppConfig
 var pathToTemplates = "./templates"
+
+func Add(a, b int) int {
+	return a + b
+}
+
+// Itearte returns a slice of ints starting at 1, going to count
+func Iterate(count int) []int {
+	var i int
+	var items []int
+	for i = 0; i < count; i++ {
+		items = append(items, i)
+	}
+
+	return items
+}
 
 // NewRenderer sets the config for the template package
 // changed the name from NewTemplates to NewRenderer (the old name could have repeated)
@@ -30,6 +48,10 @@ func NewRenderer(a *config.AppConfig) {
 // HumanDate returns time in YYYY-MM-DD format
 func HumanDate(t time.Time) string {
 	return t.Format("2006-01-02")
+}
+
+func FormatDate(t time.Time, f string) string {
+	return t.Format(f)
 }
 
 // AddDefaultData adds data for all templates
