49308bd1bd5b9382ec36df5c93a62862c84a96ee
Piotr Jaworski <piotr.jaworski@rtbhouse.com>
2023-08-25 11:14:52 +0200

    Add admin user to migrations

---
+++ migrations/20230825090858_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('Piotr','Jaworski','admin@admin.com','$2a$12$zEoqVPK0v6GzFranS/gM8.gUyfFIo9.WaGymqjQ1WT2doPQAdLClq',3,'2023-08-21 00:00:00','2023-08-21 00:00:00');
